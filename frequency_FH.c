6d677d4c1e50b76b9f6b22e6e314e623b100cd1a
vikram1003 <72300189+vikram1003@users.noreply.github.com>
2020-10-03 15:38:44 +0530

    added more programs

---
+++ frequency_FH.c
@@ -0,0 +1,43 @@
+/*
+#include<stdio.h>
+#include<string.h>
+main()
+{
+    FILE *fp;
+    char f[15],str[15];
+    int c=0;
+    printf("enter the word to be checked\n");
+    gets(f);
+    fp=fopen("file3.txt","r");
+    if(fp=NULL)
+    {
+        printf("file cannot open");
+        exit(1);
+    }
+
+    while(!feof(fp))
+    {
+         fscanf(fp,"%s",str);
+        if(strcmp(str,f)==0)
+            c++;
+
+    }
+    printf("frequency is %d ",c);
+    fclose(fp);
+}
+
+
+*/
+
+
+
+#include<stdio.h>
+#include<string.h>
+main()
+{
+    char s[]="abcd";
+    char *s1="abcd";
+    printf("%d%d%d\n",strlen(s1),strlen(s),strlen("abcd"));
+    printf("%d",strcmp("push","pull"));
+}
+
